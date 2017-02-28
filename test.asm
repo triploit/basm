7b0b1f373f7ebe211a9fe8a52e88a65d7154c51c
triploit <tristanpieper080803@gmail.com>
2017-02-28 20:57:58 +0100

    Updated to Version 0.1.5b

--- test.asm
+++ test.asm
@@ -1,63 +1,64 @@
-main:
-	var x
-	ptr ax, x		# Pointer to x
-	mov 100, ax		# 100 -> ax -> x
-	pushv x			# push x to stack
-	outl
-	ptr ax, ax		# Pointer to ax (ax -> ax)
-
-	mov 300, ax		# 300 -> ax
-	pushv x			# push x to stack (100)
-	outl
-	pop
-	push ax			# push ax to stack (300)
-	outl
-
-	mov 400, ax
-	mvv ax, x
-	pushv x
-	outl
-
-	mov 1, ax
-	mov 2, bx
-	eqi ax, bx
-	rts hx
-	outl
-	pop
-
-	push "Input: "
-	mov 7, hx;
-	jmp write_string
-	inp
-	mov hx, gx
-
-	push "Your input was: "
-	mov 17, hx
-	jmp write_string
-
+stack_to_array: # needs length of input in hx, output in list_ax
+	ags list_ax, gx
+	mov 0, fx
 	mov gx, hx
-	jmp write_string
-	push 10
 
-	ptc
-	ptc
-	pop
-
-	arr a
-	ata a, "Hello World!"
-	pushv a
-	ags a, hx
-	jmp write_string
-
-	push 10
-	ptc
-	hlt
-
-write_string:
 	.beg:
-		ptc
-		pop
 		sub 1, hx
-
+		str gx
+		asv list_ax, hx, gx
+		pop
 	ggn beg
 	return
+
+array_to_stack: # needs array in list_ax
+	ags list_ax, hx
+	mov 0, fx
+
+	.beg:
+		aga list_ax, fx, gx
+		push gx
+		sub 1, hx
+		add 1, fx
+	gnn beg
+	return
+
+write_string: # String value on Stack, Length on String in HX
+	.beg:
+		sub 1, hx
+		ptc
+		pop
+	ggn beg
+	return
+
+write_array: # String value in LIST_AX
+	ags list_ax, hx
+	mov 0, ax
+	.beg:
+		aga list_ax, ax, bx
+		push bx
+		ptc
+		pop
+		add 1, ax
+		sub 1, hx
+	ggn beg
+	return
+
+new_line: # prints new line
+	push 10
+	ptc
+	pop
+	return
+
+main:
+    var o_in
+    inp
+    mvv hx, o_in
+    jmp stack_to_array
+    jmp array_to_stack
+    pushv o_in
+    str hx
+    pop
+    jmp write_string
+    jmp new_line
+    hlt
